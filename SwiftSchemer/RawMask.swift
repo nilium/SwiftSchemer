fdb4941e8473c119a9fa21ab03f38dc3e92f5314
Noel Cower <ncower@gmail.com>
2014-07-27 15:46:43 -0700

    Add RawMask.swift
    Just adds an &== operator to return whether the bitwise-and of two
    RawRepresentables yields the RHS value.
    (e.g., same as `(foo & bar) == bar`)

---
+++ SwiftSchemer/RawMask.swift
@@ -0,0 +1,19 @@
+//
+//  RawMask.swift
+//  SwiftSchemer
+//
+//  Created by Noel Cower on 07/27/14.
+//  Copyright (c) 2014 Noel Cower. All rights reserved.
+//
+
+import Foundation
+
+
+operator infix &== { associativity none precedence 130 }
+
+
+@infix func &== <T: RawRepresentable where T.RawType: UnsignedInteger>(lhs: T, rhs: T) -> Bool {
+    let rhsRaw: T.RawType = rhs.toRaw()
+    let lhsRaw: T.RawType = rhs.toRaw()
+    return (lhsRaw & rhsRaw) == rhsRaw
+}
