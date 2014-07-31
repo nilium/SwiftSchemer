2d8bea664bf8fbdf6cfecad833d494f48a675a45
Noel Cower <ncower@gmail.com>
2014-07-31 13:24:04 -0700

    Give QScheme changes revision action names.

--- SwiftSchemer/QScheme.swift
+++ SwiftSchemer/QScheme.swift
@@ -75,59 +75,59 @@ class QScheme: NSObject {
 
     // Viewport colors (i.e., default background/foreground)
     var viewportBackground: NSColor = whiteColor() {
-        didSet { revisionTracker.addRevision { self.viewportBackground = oldValue } }
+        didSet { revisionTracker.addRevision("viewport background change") { self.viewportBackground = oldValue } }
     }
 
     var viewportForeground: NSColor = blackColor {
-        didSet { revisionTracker.addRevision { self.viewportForeground = oldValue } }
+        didSet { revisionTracker.addRevision("viewport foreground change") { self.viewportForeground = oldValue } }
     }
 
 
     // Gutter colors
     var gutterBackground: NSColor = unsetColor {
-        didSet { revisionTracker.addRevision { self.gutterBackground = oldValue } }
+        didSet { revisionTracker.addRevision("gutter background change") { self.gutterBackground = oldValue } }
     }
 
     var gutterForeground: NSColor = unsetColor {
-        didSet { revisionTracker.addRevision { self.gutterForeground = oldValue } }
+        didSet { revisionTracker.addRevision("gutter foreground change") { self.gutterForeground = oldValue } }
     }
 
 
     // Find highlight colors
     var findHighlightBackground: NSColor = unsetColor {
-        didSet { revisionTracker.addRevision { self.findHighlightBackground = oldValue } }
+        didSet { revisionTracker.addRevision("found selection change") { self.findHighlightBackground = oldValue } }
     }
 
     var findHighlightForeground: NSColor = unsetColor {
-        didSet { revisionTracker.addRevision { self.findHighlightForeground = oldValue } }
+        didSet { revisionTracker.addRevision("found text change") { self.findHighlightForeground = oldValue } }
     }
 
 
     // Editor colors
     var invisiblesForeground: NSColor = whiteColor(white: 0.75) {
-        didSet { revisionTracker.addRevision { self.invisiblesForeground = oldValue } }
+        didSet { revisionTracker.addRevision("invisibles color change") { self.invisiblesForeground = oldValue } }
     }
 
     var lineHighlight: NSColor = blackColor.colorWithAlphaComponent(0.07) {
-        didSet { revisionTracker.addRevision { self.lineHighlight = oldValue } }
+        didSet { revisionTracker.addRevision("line highlight change") { self.lineHighlight = oldValue } }
     }
 
     var caretForeground: NSColor = blackColor {
-        didSet { revisionTracker.addRevision { self.caretForeground = oldValue } }
+        didSet { revisionTracker.addRevision("caret color change") { self.caretForeground = oldValue } }
     }
 
 
     // Selection colors
     var selectionFill: NSColor = NSColor.selectedTextBackgroundColor().colorUsingSchemeColorSpace() {
-        didSet { revisionTracker.addRevision { self.selectionFill = oldValue } }
+        didSet { revisionTracker.addRevision("selection fill change") { self.selectionFill = oldValue } }
     }
 
     var selectionBorder: NSColor = NSColor.selectedTextBackgroundColor().colorUsingSchemeColorSpace().colorWithAlphaComponent(0.0) {
-        didSet { revisionTracker.addRevision { self.selectionBorder = oldValue } }
+        didSet { revisionTracker.addRevision("selection border change") { self.selectionBorder = oldValue } }
     }
 
     var inactiveSelectionFill: NSColor = NSColor.selectedTextBackgroundColor().colorUsingSchemeColorSpace().colorWithAlphaComponent(0.5) {
-        didSet { revisionTracker.addRevision { self.inactiveSelectionFill = oldValue } }
+        didSet { revisionTracker.addRevision("inactive selection change") { self.inactiveSelectionFill = oldValue } }
     }
 
 
@@ -136,7 +136,7 @@ class QScheme: NSObject {
             for rule in rules {
                 rule.revisionTracker = revisionTracker
             }
-            revisionTracker.addRevision { self.rules = oldValue }
+            revisionTracker.addRevision("rule set change") { self.rules = oldValue }
         }
     }
 
